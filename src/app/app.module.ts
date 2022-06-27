475187d4cd7c867e3533f945b686ac4c211d6c0d
Matias Lopez <Matias.Lopez@endava.com>
2022-06-27 18:02:33 -0300

    css almost done, it has a width error problem with the components and the div inside a component not matching

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GameComponent } from './game/game.component';
+import { TileComponent } from './tile/tile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameComponent,
+    TileComponent
   ],
   imports: [
     BrowserModule,
